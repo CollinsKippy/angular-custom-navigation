40c8d6781c850db333b3ca8a789047299d03b009
CollinsKippy <115981895+CollinsKippy@users.noreply.github.com>
2022-12-14 20:45:07 +0300

    1 file modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, InjectionToken, OnInit } from '@angular/core';
+
+interface Logger {
+  log: (val: string) => void;
+}
+
+const LOGGER_TOKEN = new InjectionToken<Logger>('MyLoggerToken', {
+  providedIn: 'root',
+  factory: () => {
+    return {
+      log: (val: string): void => console.log(val),
+    };
+  },
+});
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
